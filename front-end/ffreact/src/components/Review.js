45cc9bfbda75e4c8b264abe2fd07c7d9b6030cad
Matthew Ciarletta <m.r.ciarletta@gmail.com>
2023-10-05 12:25:10 -0400

    fixed review key issue

--- front-end/ffreact/src/components/Review.js
+++ front-end/ffreact/src/components/Review.js
@@ -1,20 +1,20 @@
 import { Row } from "react-bootstrap";
 
 export default function Review({ review }) {
-
   return (
     <>
       <h1>Reviews:</h1>
-      {review.map((rev => (
-        <>
-        <h6>Title: {rev.title} </h6>
-        <p>{rev.reviewBody}</p>
-        <p>Score: {rev.rating} </p>
-        <p>Contributor: {rev.contributor} </p>
-        <p>Posted: {rev.datePosted} </p>
-        </>
-      )))}
-      
+      {review.map((rev) => (
+        
+          <div key={rev.id}>
+            <h6>Title: {rev.title} </h6>
+            <p>{rev.reviewBody}</p>
+            <p>Score: {rev.rating} </p>
+            <p>Contributor: {rev.contributor} </p>
+            <p>Posted: {rev.datePosted} </p>
+          </div>
+        
+      ))}
     </>
   );
 }
